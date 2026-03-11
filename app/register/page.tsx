69c37dee1705a9ca84e030513921e91720b42523
Saksham Verma <sakshamverma1000@gmail.com>
2026-03-12 00:05:03 +0530

    Added a prisma.config.ts file, changed prisma version from 6.17.1 to 7.5.something

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,47 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {authClient} from "@/lib/auth-client";
+
+// DATA FLOW:
+/*
+    From here to /api/auth/[...all] route.
+    From above file to auth.ts, then to database using PrismaClient.
+
+*/
+
 export default function Register(){
 
+    const handleSignUp = async ()=>{
+        
+        const {data,error} = await authClient.signUp.email({
+            email: "dummy@gmail.com",
+            password: "123dummyPass",
+            name: "Saksham Verma",
+            image: "cloudinary_URL",
+            callbackURL: "/profile",
+        },{
+
+        onRequest: (ctx)=>{
+            // show loading spinner.
+        },
+
+        onSuccess:()=>{
+            //redirect to /profile route.
+        },
+
+        onError:(ctx)=>{
+            alert(ctx.error.message);
+        },
+
+        } 
+
+    );
+
+
+    }
     return(
 
-        
+        <Button onClick={handleSignUp}>Register</Button>
+
     );
 }
